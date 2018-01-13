b036e58ca97c259c80fccff75f37edd15351523e
Rajasekhar Karanam <karanam.rajasekhar44@gmail.com>
2018-01-13 23:35:50 +0530

    DOM Crash Course by Traversy: Part 1

---
+++ DOM Crash Course/dom.js
@@ -0,0 +1,29 @@
+// Examine the document object
+
+// console.dir(document);
+
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+
+// document.title = "Raja Sekhar K";
+
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+
+// console.log(document.all);
+
+// console.log(document.all[10]);
+
+// document.all[10].textContent = "Changed Header";
+
+// console.log(document.links);
+// console.log(document.forms);
+
+// getElementById //
+var headerTitle = document.getElementById('header-title');
+console.log(headerTitle);
+
+headerTitle.textContent = "Get Element By Id";
+
