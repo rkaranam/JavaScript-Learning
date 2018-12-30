3b8de0b643c2aad47cf4fef62deae3f717a2ff5e
Rajasekhar Karanam <karanam.rajasekhar44@gmail.com>
2018-12-30 16:49:12 +0530

    JS Booklist: Alerts and Validation

--- js-booklist-app/app.js
+++ js-booklist-app/app.js
@@ -43,6 +43,20 @@ class UI {
         }
     }
 
+    static showAlert(message, className) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${className} text-center`;
+        const textNode = document.createTextNode(message);
+        div.appendChild(textNode);
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+        // Vanish alert message after 2 seconds
+        setTimeout(function() {
+            document.querySelector('.alert').remove();
+        }, 2000);
+    }
+
     static clearFields() {
         document.querySelector('#title').value = '';
         document.querySelector('#author').value = '';
@@ -65,17 +79,26 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector('#isbn').value;
 
-    // Instantiate a book
-    const book = new Book(title, author, isbn);
+    if(title === '' || author === '' || isbn === '') {
+        UI.showAlert("Please fill all details", 'danger');
+    } else {
+        // Instantiate a book
+        const book = new Book(title, author, isbn);
 
-    // Add book to UI
-    UI.addBookToList(book);
+        // Add book to UI
+        UI.addBookToList(book);
 
-    // Clear Fields
-    UI.clearFields();
+        // Show success message
+        UI.showAlert('Book Added', 'success');
+
+        // Clear Fields
+        UI.clearFields();
+    }
+    
 });
 
 // Event: Delete a book
 document.querySelector('#book-list').addEventListener('click', (e) => {
     UI.deleteBook(e.target);
+    UI.showAlert('Book Deleted', 'success');
 });
